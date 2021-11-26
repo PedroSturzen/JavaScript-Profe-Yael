45502dd56463170a4b1988aac2a79a2443c11400
Pedro Sturzen <pedrosturzen@gmail.com>
2021-11-26 00:18:21 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let nombre = prompt ("Ingresa tu nombre");
+let apellido = prompt ("Ingresa tu apellido");
+let nombreApellido = nombre + " " + apellido;
+
+alert ("Nombre completo: " + nombreApellido);
+console.log("Nombre completo: " + nombreApellido);
+document.write("Tu nombre completo es " + nombreApellido);
