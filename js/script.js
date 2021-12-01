7d4ef0e0449e0588c4020fc8fe03dfb9e7e15a1b
Pedro Sturzen <pedrosturzen@gmail.com>
2021-12-01 14:56:56 -0300

    Clase2 PedroSturzen

--- js/script.js
+++ js/script.js
@@ -1,7 +1,45 @@
-let nombre = prompt ("Ingresa tu nombre");
-let apellido = prompt ("Ingresa tu apellido");
-let nombreApellido = nombre + " " + apellido;
+// ? Ejemplo Edad (Clase)
+/*
+let edad = prompt('Cual es tu edad?')
 
-alert ("Nombre completo: " + nombreApellido);
-console.log("Nombre completo: " + nombreApellido);
-document.write("Tu nombre completo es " + nombreApellido);
+if (edad >= 18){
+    alert('Sos mayor de edad');
+}
+else if (edad < 18){
+    alert('Sos menos de edad');
+}
+else{
+    alert('Sos inducomentado');
+}
+*/
+
+// ? Desafio Clase 2
+/* Remeras */
+let precioRemera = prompt('Para vos, cuanto valen las remeras?');
+
+if ((precioRemera < 650) || (precioRemera > 2300)){
+    alert('No estan eso!!! Estan entre 650 pesos y 2300');
+    console.log('No esta entre el rango de precio');
+}
+else if((precioRemera => 650) || (precioRemera <= 2300)){
+    alert('Sii !! generalmente estan eso hoy en dia las remeras')
+    console.log('Bien! esta en el rango de precio');
+}
+else{
+    alert('No se que pusiste pero me exploto el cerebro.');
+}
+
+/* Short */
+let precioShort = prompt('Para vos, cuanto valen los shorts?');
+
+if ((precioShort < 700) || (precioShort > 1300)){
+    alert('No estan eso!!! Los shorts estan entre 700 pesos y 1300');
+    console.log('No esta entre el rango de precio');
+}
+else if((precioShort => 700) || (precioShort <= 1300)){
+    alert('Los shorts andan por ese precio, asi es!');
+    console.log('Bien! esta en el rango de precio');
+}
+else{
+    alert('No se que pusiste pero me exploto el cerebro.');
+}
